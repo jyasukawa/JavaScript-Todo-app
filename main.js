e6095a4797d4fdb4a5bee136499060989882ff49
jyasukawa <mikeloss999@gmail.com>
2024-10-07 10:14:53 +0900

    feat: delete button

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+const taskList = document.getElementById('task-list');
+const taskInput = document.getElementById('task-input');
+
 function addTask() {
-    const taskList = document.getElementById('task-list');
-    const taskInput = document.getElementById('task-input');
     const task = taskInput.value.trim(); //前後の不要なスペースを取り除く
-    console.log(task);
+    // console.log(task);
 
     if (task !== "") { //以降、タスクが空でない場合のみ実行
         const li = document.createElement('li'); //新しく追加するタスク用のli要素を作成
         li.textContent = task; // li要素の文字列にtaskを設定
         taskList.appendChild(li); // 親ノードに子ノードを追加
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '削除';
+        li.appendChild(deleteButton);
+        deleteButton.addEventListener('click', function() {　// 削除ボタンをクリックしたときのイベントを設定
+            taskList.removeChild(li); // 親ノード（taskList）からこのli要素を削除
+        });
+
         taskInput.value = ''; //追加ボタンを押したら、入力欄の文字が消えるようにする
     }
 }
